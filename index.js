ffcc4cff370173c447bcd9af5103b94bd2785110
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-10-21 18:40:58 +0530

    changes done

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ const io = socketio(server);
 
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+// app.set(express.static(path.join(__dirname, "public")));
+app.use('/public', express.static('public'));
+
+
+io.on("connection", function (socket){
+  console.log("Connected")
+})
 
 app.get("/", (req, res) => {
-  res.send("Hello World from server");
+  res.render("app");
 });
 
 server.listen(process.env.PORT, () => {
